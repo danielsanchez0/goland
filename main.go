94f9f1914176443c9f82fd7f8fd643f38b62b2d1
Daniel Sanchez <daniel991938@gmail.com>
2022-08-27 16:43:13 -0500

    base de datos - arreglos

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"log"
 
-func main(){
-	fmt.Println("Hola mundo")
+	"github.com/danielsanchez0/goland/handlers"
+)
+
+func main() {
+	if database.chequeoBase() == 0 {
+		log.Fatal("sin conexion a la db")
+		return
+	}
+
+	handlers.Manejadores()
 }
